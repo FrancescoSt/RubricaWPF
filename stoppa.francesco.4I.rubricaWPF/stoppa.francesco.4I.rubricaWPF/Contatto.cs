493af10c7e5ea6327e33bd4a5bd29d96cb057cf1
Francesco Adrian Stoppa <127590071+FrancescoSt@users.noreply.github.com>
2024-02-15 19:04:11 +0100

    Add files via upload

--- stoppa.francesco.4I.rubricaWPF/stoppa.francesco.4I.rubricaWPF/Contatto.cs
+++ stoppa.francesco.4I.rubricaWPF/stoppa.francesco.4I.rubricaWPF/Contatto.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,8 +11,9 @@ using System.Windows;
 namespace stoppa.francesco._4I.rubricaWPF
 {
     public enum TipoContatto { nessuno, Email, Telefono, Web, Instagram, Facebook }
-    public class Contatto { 
-    
+    public class Contatto
+    {
+
         public int idPersona { get; set; }
         public TipoContatto Tipo { get; set; }
         public string Valore { get; set; }
@@ -42,15 +44,15 @@ namespace stoppa.francesco._4I.rubricaWPF
         public string Telefono { get; set; }
         public string Cognome { get => _cognome; set => _cognome = value; }
 
-        public Contatto() 
+        public Contatto()
         {
             Tipo = TipoContatto.nessuno;
         }
 
         //Costruisce un contatto, partendo da un csv
-        public Contatto(string riga) 
+        public Contatto(string riga)
         {
-            string[] campi = riga.Split( ';' );
+            string[] campi = riga.Split(';');
 
             if (campi.Count() != 3)
             {
@@ -62,7 +64,7 @@ namespace stoppa.francesco._4I.rubricaWPF
             int.TryParse(campi[0], out id);
             idPersona = id;
 
-            
+
             //Conversione da stringa a enum
             TipoContatto c;
             Enum.TryParse(campi[1], out c);
@@ -70,32 +72,117 @@ namespace stoppa.francesco._4I.rubricaWPF
 
             this.Valore = campi[2];
 
-            }
-           
-
         }
-
-    public class Contatti : List<Contatti>
-    {
-        public Contatti() { }
-
-        public Contatti(string nomeFile)
+        public static Contatto MakeContatto(string riga)
         {
-            StreamReader ts = new StreamReader("Contatti.csv");
-            ts.ReadLine();
+            string[] campi = riga.Split(';');
 
-            while (!ts.EndOfStream)
+            TipoContatto c;
+            Enum.TryParse(campi[1], out c);
+
+            switch (c)
             {
-                base.Add(new Contatti(ts.ReadLine()));
+                case TipoContatto.Email:
+                    return new ContattoEmail(riga);
+                    break;
 
-                //dgContatti.ItemsSource = Contatti;
+                case TipoContatto.Telefono:
+                    return new ContattoNumero(riga);
+                    break;
+
+                case TipoContatto.Web:
+                    return new ContattoWeb(riga);
+                    break;
+
+                case TipoContatto.Instagram:
+                    return new ContattoInstagram(riga);
+                    break;
+
+                case TipoContatto.Facebook:
+                    return new ContattoFacebook(riga);
+                    break;
+                    
             }
+            return new Contatto(riga);
+        }
+
+
+    }
+        public class Contatti : List<Contatto>
+        {
+            public Contatti() { }
+
+            public Contatti(string nomeFile)
+            {
+                StreamReader ts = new StreamReader("Contatti.csv");
+                ts.ReadLine();
+
+                while (!ts.EndOfStream)
+                {
+                    base.Add(Contatto.MakeContatto(ts.ReadLine()));
+
+                    //dgContatti.ItemsSource = Contatti;
+                }
+            }
+
+        }
+        
+    public class ContattoEmail : Contatto
+        {
+        public ContattoEmail() { }
+
+        public ContattoEmail(string riga)
+            : base(riga)
+        { }
+
+        }
+
+    public class ContattoNumero : Contatto
+    {
+        public ContattoNumero() { }
+
+        public ContattoNumero(string riga)
+            : base(riga)
+        {
+
         }
     }
 
+    public class ContattoWeb : Contatto
+    {
+        public ContattoWeb() { }
 
-     
+        public ContattoWeb(string riga)
+            : base (riga)
+        {
+
+        }
     }
+
+    public class ContattoInstagram : Contatto
+    {
+        public ContattoInstagram() { }
+
+        public ContattoInstagram(string riga)
+            : base(riga)
+        {
+
+        }
+    }
+
+    public class ContattoFacebook : Contatto
+    {
+        public ContattoFacebook() { }
+
+        public ContattoFacebook(string riga)
+            : base(riga)
+        {
+
+        }
+
+    }
+
+}
 
 
 
